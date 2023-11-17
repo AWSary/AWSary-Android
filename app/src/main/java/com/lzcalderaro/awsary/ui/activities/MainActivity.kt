7fded3312b2210873e60f6d893081dae09f6092e
Luiz Calderaro <lzcalderaro@eml.cc>
2023-11-17 15:55:15 +0000

    Adding stuffs

--- app/src/main/java/com/lzcalderaro/awsary/ui/activities/MainActivity.kt
+++ app/src/main/java/com/lzcalderaro/awsary/ui/activities/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-/*    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }*/
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
