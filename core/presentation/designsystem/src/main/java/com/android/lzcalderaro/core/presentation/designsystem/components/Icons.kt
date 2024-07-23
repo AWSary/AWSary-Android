777be27b6b58b4b5b05826b3756f107affa4058c
Luiz Calderaro <lzcalderaro@gmail.com>
2024-07-23 20:51:30 +0100

    Remove unused icons

---
+++ core/presentation/designsystem/src/main/java/com/android/lzcalderaro/core/presentation/designsystem/components/Icons.kt
@@ -0,0 +1,10 @@
+package com.android.lzcalderaro.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+
+val ArrowLeftIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.arrow_left)
